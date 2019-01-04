bedc6c0620b6c98996217a12d492e7053d675770
Alexander <lexybiade@gmail.com>
2019-01-04 15:35:05 -0500

    animated border class 'borderBlink' created

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 
 //Coding the start button
 
-var button = document.querySelector("#start");
-var score = document.querySelectorAll(".gameContainer__display");
+var button = document.querySelector("#start"),
+ score = document.querySelectorAll(".gameContainer__display"),
+ numbs = [1,2,3],
+ numbsDOM = document.querySelectorAll(".gameContainer__numbers");
 
 Array.prototype.shuffle = function() {
     var input = this;
@@ -27,4 +29,15 @@ button.addEventListener("click", function(){
      };
    };
     tempArray.shuffle();
+
+    //create an array of the numbers to choose from
+   for(var i=0; i<numbsDOM.length; i++){
+    for(var i=0; i<numbs.length; i++){
+        numbsDOM[i].textContent=numbs[i];
+    }
+   }
+
    })
+
+  //when the page loads
+  //var score =  
